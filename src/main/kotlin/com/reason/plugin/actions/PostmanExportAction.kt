04382c0860c103892d611ca7f8868ad6e1a37972
impassive <tingfa.yang@duitang.com>
2023-06-08 17:50:07 +0800

    init

---
+++ src/main/kotlin/com/reason/plugin/actions/PostmanExportAction.kt
@@ -0,0 +1,13 @@
+package com.reason.plugin.actions
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+/**
+ * @author impassive
+ */
+open class PostmanExportAction : AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+        TODO("Not yet implemented")
+    }
+}
---
+++ src/main/kotlin/com/reason/plugin/actions/YapiExportAction.kt
@@ -0,0 +1,13 @@
+package com.reason.plugin.actions
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+/**
+ * @author impassive
+ */
+open class YapiExportAction : AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+        TODO("Not yet implemented")
+    }
+}
